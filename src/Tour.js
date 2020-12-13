0a3dd3c8af03b826e3615c79a4b55fa14260efb2
MaQueen <shusineko@icloud.com>
2020-12-13 17:25:24 +0900

    ツアーネーム作成

--- src/Tour.js
+++ src/Tour.js
@@ -4,6 +4,11 @@ const Tour = ({ id, image, info, price, name }) => {
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+        </div>
+      </footer>
     </article>
   );
 };
